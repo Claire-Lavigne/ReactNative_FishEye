4ba272349618b7e25266482ce4e6d7d472488a1c
Claire-Lavigne <lavigneclaire81@gmail.com>
2020-12-17 21:00:00 +0100

    adding some style + get datas

--- js/app.js
+++ js/app.js
@@ -19,7 +19,6 @@ fetch('./js/datas.js')
 //.catch (error => { console.log(error) })
 
 
-
 function createArticle() {
 
   const container = document.querySelector('#container > section');
@@ -30,22 +29,18 @@ function createArticle() {
     const text = document.createElement('div');
     const tags = document.createElement('div');
 
-    tags.classList.add('card__tags');
     photographer.tags.forEach(tag => {
-      tags.innerHTML += `<a>#${tag}</a>`
+      tags.innerHTML += `<a class="tag">#${tag}</a>`
     })
 
-    text.classList.add('card__text');
     text.innerHTML += `<p>${photographer.city}, ${photographer.country}</p>`
     text.innerHTML += `<p>${photographer.tagline}</p>`
     text.innerHTML += `<p>${photographer.price}€/jour</p>`
 
-    articleLink.classList.add('card__link');
     articleLink.setAttribute('aria-label', photographer.name);
+    articleLink.innerHTML += `<img src="${photographer.portrait}" alt=""/>`
     articleLink.innerHTML += `<h2>${photographer.name}</h2>`
-    articleLink.innerHTML += `<img src="${photographer.name}" alt=""/>`
 
-    article.classList.add('card');
     article.appendChild(articleLink);
     article.appendChild(text);
     article.appendChild(tags);
@@ -56,24 +51,23 @@ function createArticle() {
 createArticle()
 
 function createNav() {
-
-  const tagsArray = [];
-
-
-  photographers.forEach(photographer => {
-    tagsArray.push(...photographer.tags);
-  })
-
-  const tagsNav = [...new Set(tagsArray)];
+  const array = [];
   const nav = document.querySelector('nav');
 
-  tagsNav.forEach(tag => {
+  // add all tags from photographers object into empty array
+  photographers.forEach(photographer => {
+    array.push(...photographer.tags);
+  });
+
+  // remove duplicate tags and for each tag remaining,
+  // create elements : (a[aria-label] > span)
+  // and append them into the nav
+  [...new Set(array)].forEach(tag => {
     const a = document.createElement('a');
     const span = document.createElement('span');
-
-    span.setAttribute('role', 'link');
     span.innerHTML += `#${tag}`;
-
+    a.classList.add('tag');
+    a.setAttribute('aria-label', 'tag');
     a.appendChild(span);
     nav.appendChild(a);
   })
@@ -82,14 +76,11 @@ createNav()
 
 
 /*
-  const photographers = data.results;
-  return photographers.map(function(photographer) { // Map through the results and for each run the code below
+   return photographers.map(function(photographer) { // Map through the results and for each run the code below
     const li = createNode('li'), //  Create the elements we need
         img = createNode('img'),
         span = createNode('span');
-    img.src = photographer.picture.medium;  // Add the source of the image to be the src of the img element
-    span.innerHTML = `${photographer.name.first} ${photographer.name.last}`; // Make the HTML of our span to be the first and last name of our photographer
-    append(li, img); // Append all our elements
+   append(li, img); // Append all our elements
     append(li, span);
     append(ul, li);
   }
