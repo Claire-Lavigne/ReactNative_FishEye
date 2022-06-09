b9c5f694b09064c5e02524c5003c75a8a33e23e6
clavigne <clavigne@datasolution.fr>
2022-06-09 14:53:32 +0200

    solving bugs

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,18 @@
 import React from "react";
-import { StyleSheet, Image, Text, Button, View } from "react-native";
+import { StyleSheet, Image, Text, TouchableOpacity, View } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import Tags from "./Tags";
-import { NavigationEvents } from "react-navigation";
 
 const Card = ({ dataPhotographers }) => {
   const navigation = useNavigation();
   console.log(dataPhotographers);
+
   return dataPhotographers.map((item) => (
     <View key={`card-${item.id}`} className={item.tags.join(" ")}>
-      <Button
-        title="Go to Photograph"
+      {console.log(
+        `https://claire-lavigne.github.io/ClaireLavigne_6_09122020/assets/Photographers_ID_Photos/${item.portrait}`
+      )}
+      <TouchableOpacity
         style={styles.link}
         onPress={() => navigation.navigate("Photograph")}
         // href={`/photographer.html?id=${item.id}`}
@@ -18,15 +20,12 @@ const Card = ({ dataPhotographers }) => {
       >
         <Image
           source={{
-            uri: `http://localhost:19006/assets/Photographers_ID_Photos/${item.portrait}`,
+            uri: `https://claire-lavigne.github.io/ClaireLavigne_6_09122020/assets/Photographers_ID_Photos/${item.portrait}`,
           }}
-          style={{
-            width: 100,
-            height: 100,
-          }}
+          style={styles.image}
         />
         <Text style={styles.subtitle}>{item.name}</Text>
-      </Button>
+      </TouchableOpacity>
       <View>
         <Text style={styles.location}>
           {item.city}, {item.country}
@@ -53,6 +52,8 @@ const styles = StyleSheet.create({
     resizeMode: "cover",
     borderRadius: "50%",
     marginBottom: 16,
+    width: 100,
+    height: 100,
   },
   subtitle: {
     color: "#d35735",
