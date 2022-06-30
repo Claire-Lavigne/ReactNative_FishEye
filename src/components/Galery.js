6ea0d8078408b09cdcb07bc2f60870110def8104
clavigne <clavigne@datasolution.fr>
2022-06-30 10:46:38 +0200

    heart double tap in progress

--- src/components/Galery.js
+++ src/components/Galery.js
@@ -1,22 +1,52 @@
-import React from "react";
-import { StyleSheet, Image, Text, View } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Image,
+  Text,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
 
 const Galery = ({ media }) => {
+  const [liked, setLiked] = useState(false);
+
+  const toggleLike = () => {
+    console.log("before", liked);
+    setLiked(!liked);
+    console.log("after", liked);
+  };
+  let lastTap = null;
+
+  const handleDoubleTap = () => {
+    const now = Date.now();
+    const DOUBLE_PRESS_DELAY = 300;
+    if (lastTap && now - lastTap < DOUBLE_PRESS_DELAY) {
+      toggleLike();
+    } else {
+      lastTap = now;
+    }
+  };
+
   return media.map(
     (item) =>
       item.image !== undefined && (
-        <View style={styles.mediaWrapper} key={`media-${item.id}`}>
-          <Image
-            source={{
-              uri: `https://claire-lavigne.github.io/ClaireLavigne_6_09122020/assets/${item.photographerId}/${item.image}`,
-            }}
-            style={styles.media}
-          />
-          <View style={styles.description}>
-            <Text>{item.alt}</Text>
-            <Text>{item.likes}</Text>
+        <TouchableWithoutFeedback
+          key={`media-${item.id}`}
+          onPress={() => handleDoubleTap()}
+        >
+          <View style={styles.mediaWrapper}>
+            <Image
+              source={{
+                uri: `https://claire-lavigne.github.io/ClaireLavigne_6_09122020/assets/${item.photographerId}/${item.image}`,
+              }}
+              style={styles.media}
+            />
+            <View style={styles.description}>
+              <Text>{item.alt}</Text>
+              <Text>{item.likes}</Text>
+            </View>
           </View>
-        </View>
+        </TouchableWithoutFeedback>
       )
   );
 };
